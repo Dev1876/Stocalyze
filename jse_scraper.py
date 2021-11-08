37cb6c3cd2daa7172f50947b77c38fc55d63dfe5
Dev876 <llewelynbenjamin@gmail.com>
2021-11-07 22:38:22 -0800

    chrome headless mode

--- jse_scraper.py
+++ jse_scraper.py
@@ -28,13 +28,13 @@ class jse_scraper(object):
 
     
 
-#    chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN")
-#    chrome_options.add_argument("--headless")
-#    chrome_options.add_argument("--disable-dev-shm-usage")
-#    chrome_options.add_argument("--no-sandbox")
-#    driver = webdriver.Chrome(executable_path=os.getenv("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+   chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN")
+   chrome_options.add_argument("--headless")
+   chrome_options.add_argument("--disable-dev-shm-usage")
+   chrome_options.add_argument("--no-sandbox")
+   driver = webdriver.Chrome(executable_path=os.getenv("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-   driver = webdriver.Chrome('driver\chromedriver.exe')
+   #driver = webdriver.Chrome('driver\chromedriver.exe')
    base_url = "https://www.jamstockex.com/market-data/"
    instruments_url = "instruments/?symbol="
    listed_companies_url = "listed-companies/"
